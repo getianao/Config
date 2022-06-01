ca0dfaa0252fe66572a8cfb906c852ef2d0d393c
Tianao Ge <getianao@gmail.com>
2022-06-01 13:15:23 +0800

    Update hip.sh

--- home/spack-env/hip.sh
+++ home/spack-env/hip.sh
@@ -1,1 +1,2 @@
-spack load hip@4.3.1 rocminfo@4.3.1%gcc@8.5.0 rocm-smi@4.3.1
+spack load hip@4.3.1 rocminfo@4.3.1%gcc@8.5.0 rocprofiler-dev@4.3.1 rocm-smi-lib@4.3.1%gcc@8.5.0
+export LD_LIBRARY_PATH=~/lib/hsa-amd-aqlprofile4.3.1_1.0.0_amd64/opt/rocm-4.3.1/lib:$LD_LIBRARY_PATH
